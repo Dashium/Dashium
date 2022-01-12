9986b06476cb28e6dee0168957790e9a2d294e02
Tai Studio <tai.studio@outlook.fr>
2022-01-12 19:49:39 +0100

    update

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+});
